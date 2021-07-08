a4fd78553fdb8fa17dbdcfb84ca53e55f358ec14
karol-janik <karol.janik@wpengine.com>
2021-07-08 11:52:03 +0200

    delete useless files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import './index.css';
-import { render } from 'react-dom';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import './index.css'
+import { render } from 'react-dom'
 
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   useQuery,
-  gql
-} from "@apollo/client";
-
+  gql,
+} from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'https://headlesspl.wpengine.com/graphql/',
@@ -19,8 +16,7 @@ const client = new ApolloClient({
   fetchOptions: {
     mode: 'no-cors',
   },
-});
-
+})
 
 // client
 //   .query({
@@ -36,52 +32,43 @@ const client = new ApolloClient({
 //     `
 //   })
 //   .then(result => console.log(result));
-  
-const POSTS_QUERY = gql `
+
+const POSTS_QUERY = gql`
   query MyQuery {
-          posts {
-            nodes {
-              title
-              content
-            }
-          }
-       }
-`;
+    posts {
+      nodes {
+        title
+        content
+      }
+    }
+  }
+`
 
 function Posts() {
+  const { loading, error, data } = useQuery(POSTS_QUERY)
 
-  const { loading, error, data } = useQuery(POSTS_QUERY);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  return data.posts.nodes.map(({title,content}) => (
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+  return data.posts.nodes.map(({ title, content }) => (
     <div key={title}>
-        <h3>{title}</h3>
-        <p>{content.replace(/<\/?[^>]+(>|$)/g, "")}</p>
+      <h3>{title}</h3>
+      <p>{content.replace(/<\/?[^>]+(>|$)/g, '')}</p>
     </div>
   ))
 }
 
+function Hello() {
+  return (
+    <div>
+      <h2>Fetching posts from WordPress 🚀</h2>
+    </div>
+  )
+}
 
-
-
-  function Hello() {
-    return (
-      <div>
-        <h2>Fetching posts from WordPress 🚀</h2>
-      </div>
-    );
-  }
-  
-  render(
-    <ApolloProvider client={client}>
-      <Hello />
-      <Posts />
-    </ApolloProvider>,
-    document.getElementById('root'),
-  );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(
+  <ApolloProvider client={client}>
+    <Hello />
+    <Posts />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
